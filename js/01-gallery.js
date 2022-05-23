8ca82b79c447256cd2eabf8b33139140f2734030
Mykhailo Khilchuk <mishahil2000@gmail.com>
2022-05-24 00:13:33 +0300

    галереї з зображеннями добавлено  модальні вікна

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js'; 
+// Change code below this line 
+ 
+// console.log(galleryItems); 
+ 
+const imgGalleryContainer = document.querySelector('.gallery'); 
+const galleryMarkup = createGalleryMarkup(galleryItems); 
+ 
+imgGalleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup); 
+imgGalleryContainer.addEventListener('click', clickOnGalleryContainer); 
+ 
+function createGalleryMarkup(galleryItems) { 
+  return galleryItems.map(({ preview, original, description }) => { 
+      return ` 
+      <div class="gallery__item"> 
+      <a class="gallery__link" href="large-image.jpg"> 
+      <img 
+      class="gallery__image" 
+      src="${preview}" 
+      data-source="${original}" 
+      alt="${description}" 
+    /> 
+  </a> 
+</div>`; 
+    }) 
+    .join(''); 
+} 
+ 
+let fragment = {}; 
+ 
+function clickOnGalleryContainer(event) { 
+  event.preventDefault(); 
+  window.addEventListener('keydown', closeModal); 
+  fragment = basicLightbox.create(`<img class="original-image" src="${event.target.dataset.source}">`, { 
+    onClose: fragment => { 
+      window.removeEventListener('keydown', closeModal); 
+    }, 
+  }); 
+  fragment.show(); 
+} 
+ 
+function closeModal(event) { 
+  if (event.code === 'Back') { 
+    window.removeEventListener('keydown', closeModal); 
+    fragment.close(); 
+  } 
+  console.log(event); 
+}
